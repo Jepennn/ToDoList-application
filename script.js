c7325dbec0bbb47e98504ccd4ce248ced067c5af
Jesper Hesselgren <jeppehesselgren@gmail.com>
2024-07-31 19:19:05 +0200

    Changes todo to be an object with completed attribute to able to save status

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ if (storedTodos) {
 
 /*--------------Helper function to make the todo list dynamic--------------*/
 
-addTodo = () => {
+function addTodo() {
     const todoText = input.value.trim();
     if (todoText.length > 0) {
-      allTodos.push(todoText);
-      updateTodoList();
-    }   
+        allTodos.push({ text: todoText, completed: false });
+        updateTodoList();
+    }
 }
 
 //Function to create a new list item for each todo
@@ -31,13 +31,13 @@ function createTodoLI (todo, todoIndex){
     const li = document.createElement('li');
 
     li.innerHTML = `<li class="todo">
-    <input type="checkbox" id="todo-${todoIndex}">
+    <input type="checkbox" id="todo-${todoIndex}" ${todo.completed ? 'checked' : ''}>
     <label class="custom-checkbox" for="todo-${todoIndex}">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
         </svg>
     </label>
     <label for="todo-${todoIndex}" class="todo-text">
-        ${todo}
+        ${todo.text}
     </label>
     <button class="delete-todo">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
@@ -57,7 +57,7 @@ function updateTodoList() {
 
         //Create a new list item (HTML) for each todo
         const todoLI = createTodoLI(todo, todoIndex);
-
+  
         //display the new todo in to the UI
         todoListToday.appendChild(todoLI);
     });
@@ -78,6 +78,12 @@ clearInputField = () => {
     input.value = '';
   }
 
+//Function to toggle the checkbox and store the state in local storage
+function toggleCheckbox(todoIndex) {
+    allTodos[todoIndex].completed = !allTodos[todoIndex].completed;
+    updateTodoList();
+}
+
 
 /*--------------Event listener for the form submission and delete items--------------*/
 
@@ -97,6 +103,15 @@ todoListToday.addEventListener('click', (e) => {
 
         //Store our todos again in local storage afer we delteted the todo we wanted to delete
         localStorage.setItem('todos', JSON.stringify(allTodos));
+    }
+});
+
+//Event listener with eventdelegation for toggle checkbox
+todoListToday.addEventListener('change', (e) => {
+    if (e.target.type === 'checkbox') {
+        const todoIndex = e.target.getAttribute('id').split('-')[1];
+        toggleCheckbox(todoIndex);
+        updateTodoList();
     }
 });
 
